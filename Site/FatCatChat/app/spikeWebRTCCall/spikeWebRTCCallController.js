cea0993cdbbf3c0ff31095f62fffaa481cf67287
David Basarab <dbasarab617@gmail.com>
2015-07-04 12:43:00 -0400

    Added spikeWebRTCCall view

---
+++ Site/FatCatChat/app/spikeWebRTCCall/spikeWebRTCCallController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myApp.spikeWebRTCCall', [])
+    .controller('spikeWebRTCCallController', [spikeWebRTCCallController]);
+
+function spikeWebRTCCallController() {
+    var viewModel = this;
+
+    viewModel.TestHello = 'Hello';
+}
+
