cbd77ed964ddaa573173ae62487875067bc72c04
David Basarab <dbasarab617@gmail.com>
2015-07-05 11:26:03 -0400

    Got simple video working.

--- Site/FatCatChat/app/spikeWebRTCCall/spikeWebRTCCallController.js
+++ Site/FatCatChat/app/spikeWebRTCCall/spikeWebRTCCallController.js
@@ -9,61 +9,8 @@ function spikeWebRTCCallController() {
     var peerConnection;
     var localStream;
 
-    viewModel.TestHello = 'Hello';
-
     viewModel.onCallClick = function() {
-        writeMessage('Call Clicked');
-
-        var audio = $('#changeMe');
-
-        var servers = null;
-
-
-        var pcConstraints = {
-            'optional': []
-        };
-
-        peerConnection = new RTCPeerConnection(servers, pcConstraints);
-
-        writeMessage('Create peerConnection');
-
-        peerConnection.onicecandidate = iceCallback;
-
-        writeMessage("Requesting local stream");
-
-        getUserMedia({
-            audio: true,
-            video: false
-        }, gotStream, function(e) {
-            alert('getUserMedia() error: ' + e.name);
-        });
     };
 
-    function writeMessage(message) {
-        var logDiv = $('#loggingData');
-
-        var paragraph = $('<p></p>');
-
-        paragraph.html(message);
-
-        logDiv.append(paragraph);
-    }
-
-    function iceCallback(event) {
-        if(event.candidate){
-
-        }
-    }
-
-    function gotStream(stream) {
-        writeMessage('Received stream');
-        localStream = stream;
-
-        var audioTracks = localStream.getAudioTracks();
-
-        if(audioTracks.length > 0) {
-            writeMessage('Using Audio device: ' audioTracks[0].label)
-        }
-    }
 }
 
