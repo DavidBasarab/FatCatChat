05b5f94ebe103bcffacc97f72997cc36bbe492b1
David Basarab <dbasarab617@gmail.com>
2015-07-05 11:59:03 -0400

    More basic video changes.

--- Site/FatCatChat/app/spikeWebRTCCall/spikeWebRTCCallController.js
+++ Site/FatCatChat/app/spikeWebRTCCall/spikeWebRTCCallController.js
@@ -1,5 +1,7 @@
 'use strict';
 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
 angular.module('myApp.spikeWebRTCCall', [])
     .controller('spikeWebRTCCallController', [spikeWebRTCCallController]);
 
@@ -9,8 +11,44 @@ function spikeWebRTCCallController() {
     var peerConnection;
     var localStream;
 
-    viewModel.onCallClick = function() {
+    var constraints = {
+        video: true
     };
+
+    function successCallback(stream) {
+
+        writeMessage("On Success Callback")
+
+        localStream = stream;     // stream available to console
+
+        var video = document.querySelector("video");
+
+        video.src = window.URL.createObjectURL(localStream);
+
+        setTimeout(function() { // For some reason play must be on seperate thread or delayed otherwise get 1 picture.
+            video.play();
+        }, 10);
+    }
+
+    function errrorCallback(error) {
+        writeMessage("getUserMedia error: " + error);
+    }
+
+    viewModel.onCallClick = function() {
+        writeMessage('On Call Click');
+
+        getUserMedia(constraints, successCallback, errrorCallback);
+    };
+
+    function writeMessage(message) {
+        var paragraph = $('<p></p>');
+
+        paragraph.html(message);
+
+        var loggingDiv = $('#loggingData');
+
+        loggingDiv.append(paragraph);
+    }
 
 }
 
